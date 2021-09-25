f187cd545c49177c98ecad90fa9d66e44b1602f8
Nick Liu <geminid@protonmail.com>
2021-09-25 11:30:13 -0400

    Adds comments

--- hue.py
+++ hue.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-from typing import Union
 
 
 # Control Sequence Introducer
@@ -12,7 +11,7 @@ def csi_seq(sgr: int) -> str:
 
     Parameters
     ----------
-    sgr: str
+    sgr: int
         Select Graphic Rendition parameters
     
     Returns
@@ -24,6 +23,9 @@ def csi_seq(sgr: int) -> str:
 
 
 class Hue:
+    """Adds colors and effects to texts in console."""
+
+    # Resets all colors and effects
     RESET = csi_seq(0)
 
     def __init__(self):
@@ -39,150 +41,682 @@ class Hue:
         return text
 
     def flush(self):
+        """Flush escape sequences and texts from buffer to console."""
         print(self.reset()._join())
         self._buf.clear()
         self.reset()
 
     def err(self, text: str) -> Hue:
+        """Theme for error messages.
+
+        Parameters
+        ----------
+        text: str
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self.reset().bg_lt_red().lt_white(text).reset()
     
     def info(self, text: str) -> Hue:
+        """Theme for info messages.
+
+        Parameters
+        ----------
+        text: str
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self.reset().bg_lt_blue().lt_white(text).reset()
 
     def ok(self, text: str) -> Hue:
+        """Theme for okay messages.
+
+        Parameters
+        ----------
+        text: str
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self.reset().bg_lt_green().black(text).reset()
 
     def warn(self, text: str) -> Hue:
+        """Theme for warning messages.
+
+        Parameters
+        ----------
+        text: str
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self.reset().bg_lt_yellow().black(text).reset()
     
     def text(self, text: str) -> Hue:
+        """Adds text to buffer.
+
+        Parameters
+        ----------
+        text: str
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(text)
 
     def reset(self) -> Hue:
+        """Resets all colors and effects.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Hue.RESET)
 
     def default(self, text: str = None) -> Hue:
+        """Resets the foreground color.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Foreground.DEFAULT, text)
 
     def black(self, text: str = None) -> Hue:
+        """Resets the foreground color to black.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Foreground.BLACK, text)
 
     def red(self, text: str = None) -> Hue:
+        """Resets the foreground color to red.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Foreground.RED, text)
 
     def green(self, text: str = None) -> Hue:
+        """Resets the foreground color to green.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Foreground.GREEN, text)
 
     def yellow(self, text: str = None) -> Hue:
+        """Resets the foreground color to yellow.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Foreground.YELLOW, text)
 
     def blue(self, text: str = None) -> Hue:
+        """Resets the foreground color to blue.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Foreground.BLUE, text)
 
     def magenta(self, text: str = None) -> Hue:
+        """Resets the foreground color to magenta.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Foreground.MAGENTA, text)
 
     def cyan(self, text: str = None) -> Hue:
+        """Resets the foreground color to cyan.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Foreground.CYAN, text)
 
     def white(self, text: str = None) -> Hue:
+        """Resets the foreground color to white.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Foreground.WHITE, text)
 
     def lt_black(self, text: str = None) -> Hue:
+        """Resets the foreground color to light black.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Foreground.LT_BLACK, text)
 
     def lt_red(self, text: str = None) -> Hue:
+        """Resets the foreground color to light red.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Foreground.LT_RED, text)
 
     def lt_green(self, text: str = None) -> Hue:
+        """Resets the foreground color to light green.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Foreground.LT_GREEN, text)
 
     def lt_yellow(self, text: str = None) -> Hue:
+        """Resets the foreground color to light yellow.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Foreground.LT_YELLOW, text)
 
     def lt_blue(self, text: str = None) -> Hue:
+        """Resets the foreground color to light blue.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Foreground.LT_BLUE, text)
 
     def lt_magenta(self, text: str = None) -> Hue:
+        """Resets the foreground color to light magenta.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Foreground.LT_MAGENTA, text)
 
     def lt_cyan(self, text: str = None) -> Hue:
+        """Resets the foreground color to light cyan.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Foreground.LT_CYAN, text)
 
     def lt_white(self, text: str = None) -> Hue:
+        """Resets the foreground color to light white.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Foreground.LT_WHITE, text)
 
     def bg_default(self, text: str = None) -> Hue:
+        """Resets the background color.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Background.DEFAULT, text)
 
     def bg_black(self, text: str = None) -> Hue:
+        """Resets the background color to black.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Background.BLACK, text)
 
     def bg_red(self, text: str = None) -> Hue:
+        """Resets the background color to red.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Background.RED, text)
 
     def bg_green(self, text: str = None) -> Hue:
+        """Resets the background color to green.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Background.GREEN, text)
 
     def bg_yellow(self, text: str = None) -> Hue:
+        """Resets the background color to yellow.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Background.YELLOW, text)
 
     def bg_blue(self, text: str = None) -> Hue:
+        """Resets the background color to blue.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Background.BLUE, text)
 
     def bg_magenta(self, text: str = None) -> Hue:
+        """Resets the background color to magenta.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Background.MAGENTA, text)
 
     def bg_cyan(self, text: str = None) -> Hue:
+        """Resets the background color to cyan.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Background.CYAN, text)
 
     def bg_white(self, text: str = None) -> Hue:
+        """Resets the background color to white.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Background.WHITE, text)
 
     def bg_lt_black(self, text: str = None) -> Hue:
+        """Resets the background color to light black.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Background.LT_BLACK, text)
 
     def bg_lt_red(self, text: str = None) -> Hue:
+        """Resets the background color to light red.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Background.LT_RED, text)
 
     def bg_lt_green(self, text: str = None) -> Hue:
+        """Resets the background color to light green.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Background.LT_GREEN, text)
 
     def bg_lt_yellow(self, text: str = None) -> Hue:
+        """Resets the background color to light yellow.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Background.LT_YELLOW, text)
 
     def bg_lt_blue(self, text: str = None) -> Hue:
+        """Resets the background color to light blue.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Background.LT_BLUE, text)
 
     def bg_lt_magenta(self, text: str = None) -> Hue:
+        """Resets the background color to light magenta.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Background.LT_MAGENTA, text)
 
     def bg_lt_cyan(self, text: str = None) -> Hue:
+        """Resets the background color to light cyan.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Background.LT_CYAN, text)
 
     def bg_lt_white(self, text: str = None) -> Hue:
+        """Resets the background color to light white.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Background.LT_WHITE, text)
 
     def bold(self, text: str = None) -> Hue:
+        """Increases font intensity.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Decoration.BOLD, text)
 
     def underline(self, text: str = None) -> Hue:
+        """Adds the underline effect.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Decoration.UNDERLINE, text)
 
     def reversed(self, text: str = None) -> Hue:
+        """Flips the foreground and background colors.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return self._add(Decoration.REVERSED, text)
 
     def _add(self, *argv: str) -> Hue:
+        """Adds escape sequences and texts to buffer.
+
+        Parameters
+        ----------
+        text: str, optional
+            Raw text.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         for arg in argv:
             if arg:
                 self._buf.append(arg)
         return self
 
     def _join(self) -> str:
+        """Joins escape sequences with texts.
+
+        Returns
+        -------
+        Hue
+            Current instance.
+        """
         return ''.join(self._buf)
 
 
 class Foreground:
+    """Foreground CSI sequences."""
     BLACK = csi_seq(30)
     RED = csi_seq(31)
     GREEN = csi_seq(32)
@@ -203,6 +737,7 @@ class Foreground:
 
 
 class Background:
+    """Background CSI sequences."""
     BLACK = csi_seq(40)
     RED = csi_seq(41)
     GREEN = csi_seq(42)
@@ -223,6 +758,7 @@ class Background:
 
 
 class Decoration:
+    """Decoration CSI sequences."""
     BOLD = csi_seq(1)
     UNDERLINE = csi_seq(4)
     REVERSED = csi_seq(7)
