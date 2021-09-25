293405f96882dc245201dbccc369f11de313a814
Nick Liu <geminid@protonmail.com>
2021-09-25 02:40:54 -0400

    rename functions

--- hue.py
+++ hue.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from typing import Union
 
 
 # Control Sequence Introducer
@@ -6,7 +7,7 @@ CSI = '\u001b['
 FIN = 'm'
 
 
-def csi_seq(sgr):
+def csi_seq(sgr: int) -> str:
     """Returns a CSI sequence.
 
     Parameters
@@ -24,156 +25,160 @@ def csi_seq(sgr):
 
 class Hue:
     RESET = csi_seq(0)
-    BANNER_LEN = 6
 
     def __init__(self):
         self._buf = []
         self.reset()
 
-    def info(self, text=None):
-        return self.lt_white().lt_blue_bg()._add_banner('INFO', text)
+    def __repr__(self) -> str:
+        return f'{self.__class__}({self.__dict__})'
 
-    def fail(self, text=None):
-        return self.lt_white().lt_red_bg()._add_banner('FAIL', text)
+    def __str__(self) -> str:
+        text = self.reset()._join()
+        self._buf.pop()
+        return text
 
-    def warn(self, text=None):
-        return self.black().lt_yellow_bg()._add_banner('WARN', text)
+    def flush(self):
+        print(self.reset()._join())
+        self._buf.clear()
+        self.reset()
 
-    def ok(self, text=None):
-        return self.black().lt_green_bg()._add_banner('OK', text)
+    def err(self, text: str) -> Hue:
+        return self.reset().bg_lt_red().lt_white(text).reset()
+    
+    def info(self, text: str) -> Hue:
+        return self.reset().bg_lt_blue().lt_white(text).reset()
 
-    def black(self, text=None):
-        return self._add(Foreground.BLACK, text)
+    def ok(self, text: str) -> Hue:
+        return self.reset().bg_lt_green().black(text).reset()
 
-    def red(self, text=None):
-        return self._add(Foreground.RED, text)
+    def warn(self, text: str) -> Hue:
+        return self.reset().bg_lt_yellow().black(text).reset()
+    
+    def text(self, text: str) -> Hue:
+        return self._add(text)
 
-    def green(self, text=None):
-        return self._add(Foreground.GREEN, text)
-
-    def yellow(self, text=None):
-        return self._add(Foreground.YELLOW, text)
-
-    def blue(self, text=None):
-        return self._add(Foreground.BLUE, text)
-
-    def magenta(self, text=None):
-        return self._add(Foreground.MAGENTA, text)
-
-    def cyan(self, text=None):
-        return self._add(Foreground.CYAN, text)
-
-    def white(self, text=None):
-        return self._add(Foreground.WHITE, text)
-
-    def lt_black(self, text=None):
-        return self._add(Foreground.LT_BLACK, text)
-
-    def lt_red(self, text=None):
-        return self._add(Foreground.LT_RED, text)
-
-    def lt_green(self, text=None):
-        return self._add(Foreground.LT_GREEN, text)
-
-    def lt_yellow(self, text=None):
-        return self._add(Foreground.LT_YELLOW, text)
-
-    def lt_blue(self, text=None):
-        return self._add(Foreground.LT_BLUE, text)
-
-    def lt_magenta(self, text=None):
-        return self._add(Foreground.LT_MAGENTA, text)
-
-    def lt_cyan(self, text=None):
-        return self._add(Foreground.LT_CYAN, text)
-
-    def lt_white(self, text=None):
-        return self._add(Foreground.LT_WHITE, text)
-
-    def black_bg(self, text=None):
-        return self._add(Background.BLACK, text)
-
-    def red_bg(self, text=None):
-        return self._add(Background.RED, text)
-
-    def green_bg(self, text=None):
-        return self._add(Background.GREEN, text)
-
-    def yellow_bg(self, text=None):
-        return self._add(Background.YELLOW, text)
-
-    def blue_bg(self, text=None):
-        return self._add(Background.BLUE, text)
-
-    def magenta_bg(self, text=None):
-        return self._add(Background.MAGENTA, text)
-
-    def cyan_bg(self, text=None):
-        return self._add(Background.CYAN, text)
-
-    def white_bg(self, text=None):
-        return self._add(Background.WHITE, text)
-
-    def lt_black_bg(self, text=None):
-        return self._add(Background.LT_BLACK, text)
-
-    def lt_red_bg(self, text=None):
-        return self._add(Background.LT_RED, text)
-
-    def lt_green_bg(self, text=None):
-        return self._add(Background.LT_GREEN, text)
-
-    def lt_yellow_bg(self, text=None):
-        return self._add(Background.LT_YELLOW, text)
-
-    def lt_blue_bg(self, text=None):
-        return self._add(Background.LT_BLUE, text)
-
-    def lt_magenta_bg(self, text=None):
-        return self._add(Background.LT_MAGENTA, text)
-
-    def lt_cyan_bg(self, text=None):
-        return self._add(Background.LT_CYAN, text)
-
-    def lt_white_bg(self, text=None):
-        return self._add(Background.LT_WHITE, text)
-
-    def bold(self, text=None):
-        return self._add(Decoration.BOLD, text)
-
-    def underline(self, text=None):
-        return self._add(Decoration.UNDERLINE, text)
-
-    def reversed(self, text=None):
-        return self._add(Decoration.REVERSED, text)
-
-    def reset(self):
+    def reset(self) -> Hue:
         return self._add(Hue.RESET)
 
-    def reset_fg(self):
-        return self._add(Foreground.RESET)
+    def default(self, text: str = None) -> Hue:
+        return self._add(Foreground.DEFAULT, text)
 
-    def reset_bg(self):
-        return self._add(Background.RESET)
+    def black(self, text: str = None) -> Hue:
+        return self._add(Foreground.BLACK, text)
 
-    def text(self, text=None):
-        return self._add(text=text)
+    def red(self, text: str = None) -> Hue:
+        return self._add(Foreground.RED, text)
 
-    def _add(self, csi_seq=None, text=None):
-        if csi_seq:
-            self._buf.append(csi_seq)
-        if text:
-            self._buf.append(text)
+    def green(self, text: str = None) -> Hue:
+        return self._add(Foreground.GREEN, text)
+
+    def yellow(self, text: str = None) -> Hue:
+        return self._add(Foreground.YELLOW, text)
+
+    def blue(self, text: str = None) -> Hue:
+        return self._add(Foreground.BLUE, text)
+
+    def magenta(self, text: str = None) -> Hue:
+        return self._add(Foreground.MAGENTA, text)
+
+    def cyan(self, text: str = None) -> Hue:
+        return self._add(Foreground.CYAN, text)
+
+    def white(self, text: str = None) -> Hue:
+        return self._add(Foreground.WHITE, text)
+
+    def lt_black(self, text: str = None) -> Hue:
+        return self._add(Foreground.LT_BLACK, text)
+
+    def lt_red(self, text: str = None) -> Hue:
+        return self._add(Foreground.LT_RED, text)
+
+    def lt_green(self, text: str = None) -> Hue:
+        return self._add(Foreground.LT_GREEN, text)
+
+    def lt_yellow(self, text: str = None) -> Hue:
+        return self._add(Foreground.LT_YELLOW, text)
+
+    def lt_blue(self, text: str = None) -> Hue:
+        return self._add(Foreground.LT_BLUE, text)
+
+    def lt_magenta(self, text: str = None) -> Hue:
+        return self._add(Foreground.LT_MAGENTA, text)
+
+    def lt_cyan(self, text: str = None) -> Hue:
+        return self._add(Foreground.LT_CYAN, text)
+
+    def lt_white(self, text: str = None) -> Hue:
+        return self._add(Foreground.LT_WHITE, text)
+
+    def bg_default(self, text: str = None) -> Hue:
+        return self._add(Background.DEFAULT, text)
+
+    def bg_black(self, text: str = None) -> Hue:
+        return self._add(Background.BLACK, text)
+
+    def bg_red(self, text: str = None) -> Hue:
+        return self._add(Background.RED, text)
+
+    def bg_green(self, text: str = None) -> Hue:
+        return self._add(Background.GREEN, text)
+
+    def bg_yellow(self, text: str = None) -> Hue:
+        return self._add(Background.YELLOW, text)
+
+    def bg_blue(self, text: str = None) -> Hue:
+        return self._add(Background.BLUE, text)
+
+    def bg_magenta(self, text: str = None) -> Hue:
+        return self._add(Background.MAGENTA, text)
+
+    def bg_cyan(self, text: str = None) -> Hue:
+        return self._add(Background.CYAN, text)
+
+    def bg_white(self, text: str = None) -> Hue:
+        return self._add(Background.WHITE, text)
+
+    def bg_lt_black(self, text: str = None) -> Hue:
+        return self._add(Background.LT_BLACK, text)
+
+    def bg_lt_red(self, text: str = None) -> Hue:
+        return self._add(Background.LT_RED, text)
+
+    def bg_lt_green(self, text: str = None) -> Hue:
+        return self._add(Background.LT_GREEN, text)
+
+    def bg_lt_yellow(self, text: str = None) -> Hue:
+        return self._add(Background.LT_YELLOW, text)
+
+    def bg_lt_blue(self, text: str = None) -> Hue:
+        return self._add(Background.LT_BLUE, text)
+
+    def bg_lt_magenta(self, text: str = None) -> Hue:
+        return self._add(Background.LT_MAGENTA, text)
+
+    def bg_lt_cyan(self, text: str = None) -> Hue:
+        return self._add(Background.LT_CYAN, text)
+
+    def bg_lt_white(self, text: str = None) -> Hue:
+        return self._add(Background.LT_WHITE, text)
+
+    def bold(self, text: str = None) -> Hue:
+        return self._add(Decoration.BOLD, text)
+
+    def underline(self, text: str = None) -> Hue:
+        return self._add(Decoration.UNDERLINE, text)
+
+    def reversed(self, text: str = None) -> Hue:
+        return self._add(Decoration.REVERSED, text)
+
+    def _add(self, *argv: str) -> Hue:
+        for arg in argv:
+            if arg:
+                self._buf.append(arg)
         return self
 
-    def _add_banner(self, banner, text=None):
-        self.text(banner.center(Hue.BANNER_LEN)).reset()
-        if text:
-            return self._add(' ', text)
-        return self
-
-    def __str__(self):
-        self.reset()
+    def _join(self) -> str:
         return ''.join(self._buf)
 
 
@@ -194,7 +199,7 @@ class Foreground:
     LT_MAGENTA = csi_seq(95)
     LT_CYAN = csi_seq(96)
     LT_WHITE = csi_seq(97)
-    RESET = csi_seq(39)
+    DEFAULT = csi_seq(39)
 
 
 class Background:
@@ -214,7 +219,7 @@ class Background:
     LT_MAGENTA = csi_seq(105)
     LT_CYAN = csi_seq(106)
     LT_WHITE = csi_seq(107)
-    RESET = csi_seq(49)
+    DEFAULT = csi_seq(49)
 
 
 class Decoration:
